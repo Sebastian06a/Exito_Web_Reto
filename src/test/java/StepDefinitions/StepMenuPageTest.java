bcd461fccc816db6312dc3b6c85491e59a66bc0b
sebastian <sebastian.raymundo.r.@gmail.com>
2024-12-10 03:58:49 -0500

    Actualización en StepMenuPageTest.java

--- src/test/java/StepDefinitions/StepMenuPageTest.java
+++ src/test/java/StepDefinitions/StepMenuPageTest.java
@@ -4,17 +4,18 @@ import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+
 
 public class StepMenuPageTest {
 
@@ -84,15 +85,6 @@ public class StepMenuPageTest {
     }
 
 
-
-//    @Y("selecciono el numero de paginacion {int}")
-//    public void seleccionoElNumeroDePaginacion(int arg0) throws InterruptedException {
-//
-//        JavascriptExecutor executor = (JavascriptExecutor) driver;
-//        executor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//*[@id=\"__next\"]/main/section[4]/div/div[2]/div[2]/div[3]/section/div/ul/li[5]/button")));
-
-
-
     @Y("le doy clic al boton de agregar producto")
     public void leDoyClicAlBotonDeAgregarProducto() throws InterruptedException {
         JavascriptExecutor executor0 = (JavascriptExecutor) driver;
@@ -110,7 +102,7 @@ public class StepMenuPageTest {
     @Y("doy clic en el detalle del producto")
     public void doyClicEnElDetalleDelProducto() {
         JavascriptExecutor executor2 = (JavascriptExecutor) driver;
-        executor2.executeScript("arguments[0].click();", driver.findElement(By.xpath("//img[@data-atom-image='true' and @src='https://exitocol.vtexassets.com/arquivos/ids/22941960-109-109' and @crossorigin='anonymous']")));
+        executor2.executeScript("arguments[0].click();", driver.findElement(By.xpath("//div[@id='render-store.custom.checkout-io']//a/img")));
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -170,87 +162,89 @@ public class StepMenuPageTest {
         inputElement2.click();
     }
 
+
     @Y("ingresar correo electronico y dar clic en confirmar")
-    public void ingresarCorreoElectronicoYDarClicEnConfirmar() throws InterruptedException {
-        // Espera hasta que el campo de correo electrónico esté visible y habilitado
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement entradaElemento = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='render-store.custom.checkout-io']//input[@type='email']")));
+    public void ingresarCorreoElectronicoYDarClicEnConfirmar() {
+        try {
+            // Espera hasta que el campo de correo electrónico esté visible y habilitado
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement entradaElemento = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='render-store.custom.checkout-io']//input[@type='email']")));
 
-        // Haz clic en el elemento con JavaScript
-        JavascriptExecutor executor5 = (JavascriptExecutor) driver;
-        executor5.executeScript("arguments[0].click();", entradaElemento);
+            // Ingresa el valor  en el campo
+            entradaElemento.sendKeys("sebas@gmail.com");
 
-        // Ingresa el valor "sebas@gmail.com" en el campo
-        entradaElemento.sendKeys("sebas@gmail.com");
-        Thread.sleep(20000);
+            // Espera hasta que el botón "Confirmar" esté visible y habilitado
+            WebElement confirmarBoton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='render-store.custom.checkout-io']//form//button[@type='submit' and contains(@class, 'exito-checkout-io-0-x-preLoginActiveButton')]")));
 
-        // Espera hasta que el botón "Confirmar" esté visible y habilitado
-        WebElement confirmarBoton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='render-store.custom.checkout-io']//form//button[@type='submit' and contains(@class, 'exito-checkout-io-0-x-preLoginActiveButton')]")));
+            // Haz clic en el botón "Confirmar" con JavaScript
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", confirmarBoton);
 
-        // Haz clic en el botón "Confirmar" con JavaScript
-        executor5.executeScript("arguments[0].click();", confirmarBoton);
-
-        // También intenta hacer clic directamente por Selenium (respaldo)
-        confirmarBoton.click();
+            // También intenta hacer clic directamente por Selenium (respaldo)
+            confirmarBoton.click();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Manejo de excepciones personalizado si es necesario
+        }
     }
 
     @Y("llenamos el formulario de ingresa tus datos")
-    public void llenamosElFormularioDeIngresaTusDatos() throws InterruptedException {
+    public void llenamosElFormularioDeIngresaTusDatos() throws InterruptedException, IOException {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Aumentar el tiempo de espera a 20 segundos
-
         // Ingresar el primer nombre
         WebElement firstNameElement = wait.until(ExpectedConditions.elementToBeClickable(By.name("firstName")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", firstNameElement);
-        Thread.sleep(6000); // Espera fija de 1 segundo
+        Thread.sleep(6000); // Espera fija de 6 segundos
         firstNameElement.click();
-        firstNameElement.sendKeys("TuNombre");
-
-        // Ingresar el apellido
+        firstNameElement.sendKeys("TuNombre"); // Ingresar el apellido
         WebElement lastNameElement = wait.until(ExpectedConditions.elementToBeClickable(By.name("lastName")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", lastNameElement);
         Thread.sleep(1000); // Espera fija de 1 segundo
         lastNameElement.click();
-        lastNameElement.sendKeys("TuApellido");
-
-        // Seleccionar el tipo de documento
+        lastNameElement.sendKeys("TuApellido"); // Seleccionar el tipo de documento
         WebElement documentTypeSelect = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-profileDocumentTypeSelect")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", documentTypeSelect);
         Thread.sleep(1000); // Espera fija de 1 segundo
-        documentTypeSelect.click();
-
-        // Seleccionar "Cédula extranjería"
+        documentTypeSelect.click(); // Seleccionar "Cédula extranjería"
         WebElement documentTypeOption = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-profileInputSelectContainer ul > li:nth-child(2)")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", documentTypeOption);
         Thread.sleep(1000); // Espera fija de 1 segundo
-        documentTypeOption.click();
-
-        // Ingresar el número de documento
+        documentTypeOption.click(); // Ingresar el número de documento
         WebElement documentElement = wait.until(ExpectedConditions.elementToBeClickable(By.name("document")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", documentElement);
         Thread.sleep(1000); // Espera fija de 1 segundo
         documentElement.click();
-        documentElement.sendKeys("123456789");
-
-        // Ingresar el número de teléfono
+        documentElement.sendKeys("123456789"); // Ingresar el número de teléfono
         WebElement phoneElement = wait.until(ExpectedConditions.elementToBeClickable(By.name("phone")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", phoneElement);
         Thread.sleep(1000); // Espera fija de 1 segundo
         phoneElement.click();
         phoneElement.sendKeys("987654321");
-
-        // Hacer clic en el primer checkbox
-        WebElement checkbox1 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-privacyPolicyAndTermsContainer .exito-checkout-io-0-x-termsContainer.exito-checkout-io-0-x-checked")));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox1);
-        Thread.sleep(1000); // Espera fija de 1 segundo
-        checkbox1.click();
-
-        // Hacer clic en el segundo checkbox
-        WebElement checkbox2 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-privacyPolicyAndTermsContainer article:nth-child(2) label div input[type='checkbox']")));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox2);
-        Thread.sleep(1000); // Espera fija de 1 segundo
-        checkbox2.click();
-    }
-}
+        try { // Verificar la existencia y visibilidad del primer checkbox
+            WebElement checkbox1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-privacyPolicyAndTermsContainer .exito-checkout-io-0-x-termsContainer.exito-checkout-io-0-x-checked")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox1);
+            wait.until(ExpectedConditions.elementToBeClickable(checkbox1));
+            checkbox1.click();
+        } catch (TimeoutException e) {
+            e.printStackTrace(); // Captura de pantalla para depuración
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File("screenshot.png"));
+            System.out.println("TimeoutException: No se pudo encontrar el elemento dentro del tiempo especificado.");
+        } catch (Exception e) {
+            e.printStackTrace(); // Manejo de excepciones personalizado si es necesario
+        }
+        try { // Verificar la existencia y visibilidad del segundo checkbox
+            WebElement checkbox2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#render-store\\.custom\\.checkout-io .exito-checkout-io-0-x-privacyPolicyAndTermsContainer article:nth-child(2) label div input[type='checkbox']")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox2);
+            wait.until(ExpectedConditions.elementToBeClickable(checkbox2));
+            checkbox2.click();
+        } catch (TimeoutException e) {
+            e.printStackTrace(); // Captura de pantalla para depuración
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File("screenshot.png"));
+            System.out.println("TimeoutException: No se pudo encontrar el elemento dentro del tiempo especificado.");
+        } catch (Exception e) {
+            e.printStackTrace(); // Manejo de excepciones personalizado si es necesario
+            } } }
 
 
 //WebElement button = driver.findElement(By.xpath(""));
